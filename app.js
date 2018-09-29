3bbc5b3383a08a6ed7605e4efcf6b3502925cac7
g82005 <43104118+g82005@users.noreply.github.com>
2018-09-29 13:40:37 -0400

    using pug as the view engine
    Using pug as the view engine instead of ejs. Update app.js accordingly. A navigation bar with bootstrap applied.

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-var http = require('http');
-var fs = require('fs');
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
-var app = express();
+let http = require("http");
+let fs = require("fs");
+let express = require("express");
+let bodyParser = require("body-parser");
+let path = require("path");
+let app = express();
+let expressValidator = require('express-validator');
+let flash = require('connect-flash');
+let session = require('express-session');
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+// load view engine
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine', 'pug');
 
-//load view engine
-app.set('view engine','ejs');
 
 // var server = http.createServer(function(req,res){
 //   res.writeHead(200, {'Content-Type': 'text/html'});
@@ -17,42 +21,72 @@ app.set('view engine','ejs');
 // });
 
 // set css folder
-app.use(express.static(path.join(__dirname, 'css')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/home', function (req,res){
-  res.sendFile('/home.html', {root: __dirname})
+// express session middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true,
+  cookie:{ secure:true }
+}))
+
+// express messages middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
 });
 
-app.get('/', function (req,res){
-  res.sendFile('/home.html', {root: __dirname})
+// express validator middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value){
+    var namespace = param.split('.')
+    , root  = namespace.shift()
+    , formParam = root;
+
+    while(namespace.length){
+      formParam += '[' + namespace/shift() + ']';
+    }
+    return{
+      param: formParam,
+      msg: msg,
+      value: value,
+    };
+  }
+}));
+
+
+app.get("/", (req, res) =>  {
+  res.render("home", { qs: req.query });
 });
 
-app.get('/parentLogin', function (req,res){
-  res.render('parentLogin', {qs: req.query});
+app.get("/parentLogin", (req, res) =>  {
+  res.render("parentLogin", { qs: req.query });
 });
-app.post('/parentLogin', urlencodedParser, function (req,res){
+app.post("/parentLogin", urlencodedParser, (req, res) =>  {
   console.log(req.body);
-  res.render('parentLogin', {qs: req.query});
+  res.render("parentLogin", { qs: req.query });
 });
-app.get('/parentregister', function (req,res){
-  res.render('parentregister', {qs: req.query});
+app.get("/parentregister", (req, res) =>  {
+  res.render("parentregister", { qs: req.query });
 });
-app.post('/parentregister', urlencodedParser, function (req,res){
+app.post("/parentregister", urlencodedParser, (req, res) =>  {
   console.log(req.body);
-  res.render('parentregister', {qs: req.query});
+  res.render("parentregister", { qs: req.query });
 });
 
-app.get('/teacherLogin', urlencodedParser, function (req,res){
-  res.render('teacherLogin', {qs: req.query});
+app.get("/teacherLogin", urlencodedParser, (req, res) =>  {
+  res.render("teacherLogin", { qs: req.query });
 });
 
-app.post('/teacherLogin', urlencodedParser, function (req,res){
+app.post("/teacherLogin", urlencodedParser, (req, res) =>  {
   console.log(req.body);
-  res.render('teacherLogin', {qs: req.query});
+  res.render("teacherLogin", { qs: req.query });
 });
-app.get('/teacherregister', function (req,res){
-  res.render('teacherregister', {qs: req.query});
+app.get("/teacherregister", (req, res) =>  {
+  res.render("teacherregister", { qs: req.query });
 });
 
-app.listen(3000, '127.0.0.1');
-console.log('listening to port 3000');
+app.listen(3000, "127.0.0.1");
+console.log("listening to port 3000");
