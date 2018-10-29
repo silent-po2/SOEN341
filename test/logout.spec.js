a234c27c6d76ef699c4cb147f84d22ca4a7423c8
SandraRoz <Bella.roz22@gmail.com>
2018-10-29 17:12:59 -0400

    added logout test

---
+++ test/logout.spec.js
@@ -0,0 +1,23 @@
+/**
+ * This test checks that the logout process properly returns the correct status code.
+ */
+
+let app = require('../app');
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+let request = chai.request(app);
+let expect = chai.expect;
+
+describe('Logout', function() {
+  describe('When pressing logout', function() {
+    it('should succesfully logout and redirect to /login', function(done) {
+      request.get('/logout').end(function(err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res).to.redirect;
+        done();
+      });
+    });
+  });
+});
