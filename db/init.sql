cee1d1a43632a5f4f83832505cb5c4e1b7f319a3
SandraRoz <Bella.roz22@gmail.com>
2018-11-10 21:18:58 -0500

    small db edit to help tests run smoother

--- db/init.sql
+++ db/init.sql
@@ -12,53 +12,6 @@ Password varchar(32),
 Type enum('T', 'P') NOT NULL,
 primary key (Id)
 );
-
-
-create table child (
-StudentId int auto_increment primary key,
-FirstName varchar(30),
-LastName varchar(30)
-);
-
-create table isParent (
-ParentId int,
-StudentId int,
-Relation enum('M', 'F'),
-foreign key (ParentId) references user(Id),
-foreign key (StudentId) references child(StudentId),
-primary key (ParentId, StudentId)
-);
-
--- create table isTeacher (
--- TeacherId int,
--- StudentId int,
--- foreign key (TeacherId) references user(Id),
--- foreign key (StudentId) references child(StudentId),
--- primary key (TeacherId, StudentId)
--- );
-
-create table course ( 
-CourseId varchar(10) primary key,
-courseName varchar(30)
-);
-
-create table teaches (
-TeacherId int,
-CourseId varchar(30),
-foreign key (TeacherId) references user(Id),
-foreign key (CourseId) references course(CourseId),
-primary key (TeacherId, CourseId)
-);
-
-create table isRegistered (
-StudentId int,
-CourseId varchar(10),
-grade int,
-foreign key (StudentId) references child(StudentId),
-foreign key (CourseId) references course(CourseId),
-primary key (StudentID, CourseId)
-);
-
 create table thread (
 	PostId int unsigned primary key NOT NULL auto_increment,
     Post varchar(256),
@@ -77,8 +30,8 @@ create table chat (
     `From` int NOT NULL,
     `To` int NOT NULL,
 	DT DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    foreign key (`From`) references user(Id),
-	foreign key (`To`) references user(Id)
+    foreign key (`From`) references user(Id) on delete cascade,
+	foreign key (`To`) references user(Id) on delete cascade
 );
 
 create table groups (
@@ -88,13 +41,58 @@ create table groups (
     foreign key (UserId) references user(Id)
 );
 
-create table GroupChat (
+create table groupChat (
 	GroupMsgId int primary key NOT NULL auto_increment,
     GroupMsg varchar(256),
     `From` int NOT NULL,
     GroupId int NOT NULL,
 	DT DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     title varchar(255),
-	foreign key (`From`) references user(Id),
-    foreign key (GroupId) references groups(GroupId)
+	foreign key (`From`) references user(Id) on delete cascade,
+    foreign key (GroupId) references groups(GroupId) on delete cascade
 );
+
+-- create table child (
+-- StudentId int auto_increment primary key,
+-- FirstName varchar(30),
+-- LastName varchar(30)
+-- );
+
+-- create table isParent (
+-- ParentId int,
+-- StudentId int,
+-- Relation enum('M', 'F'),
+-- foreign key (ParentId) references user(Id),
+-- foreign key (StudentId) references child(StudentId),
+-- primary key (ParentId, StudentId)
+-- );
+
+-- create table isTeacher (
+-- TeacherId int,
+-- StudentId int,
+-- foreign key (TeacherId) references user(Id),
+-- foreign key (StudentId) references child(StudentId),
+-- primary key (TeacherId, StudentId)
+-- );
+
+-- create table course ( 
+-- CourseId varchar(10) primary key,
+-- courseName varchar(30)
+-- );
+
+-- create table teaches (
+-- TeacherId int,
+-- CourseId varchar(30),
+-- foreign key (TeacherId) references user(Id),
+-- foreign key (CourseId) references course(CourseId),
+-- primary key (TeacherId, CourseId)
+-- );
+
+-- create table isRegistered (
+-- StudentId int,
+-- CourseId varchar(10),
+-- grade int,
+-- foreign key (StudentId) references child(StudentId),
+-- foreign key (CourseId) references course(CourseId),
+-- primary key (StudentID, CourseId)
+-- );
